6076a9662608c28b84c6b7c5ebbbaf597b2c61b4
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-10-06 22:14:22 +0200

    update name

--- lab1/src/lab1/HolaMundo.java
+++ lab1/src/lab1/HolaMundo.java
@@ -8,5 +8,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Polska gurom");
+		
+		String name ="Kacper";
+		System.out.println("Hola, "+name);
 	}
 }
